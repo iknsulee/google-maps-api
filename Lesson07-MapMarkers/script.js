c77702bf3af2e966d3324b6b44eed9e3783256bf
iknsulee <i.knsulee@gmail.com>
2016-07-16 15:29:26 +0900

    .....

--- Lesson07-MapMarkers/script.js
+++ Lesson07-MapMarkers/script.js
@@ -1,29 +1,29 @@
-(function(window, mapster) {
+(function (window, mapster) {
 
-  // map options
-  var options = mapster.MAP_OPTIONS,
-  element = document.getElementById('map-canvas'),
-  // map
-  map = mapster.create(element, options);
+    // map options
+    var options = mapster.MAP_OPTIONS,
+        element = document.getElementById('map-canvas'),
+    // map
+        map = mapster.create(element, options);
 
-  map.addMarker(37.791350, -122.435883, true);
+    map.addMarker(37.791350, -122.435883, true);
 
-  // var marker = new google.maps.Marker({
-  //   position: {
-  //     lat: 37.791350,
-  //     lng: -122.435883
-  //   },
-  //   map: map.gMap,
-  //   icon: 'mapicons/drink.png'
-  // });
+    // var marker = new google.maps.Marker({
+    //   position: {
+    //     lat: 37.791350,
+    //     lng: -122.435883
+    //   },
+    //   map: map.gMap,
+    //   icon: 'mapicons/drink.png'
+    // });
 
-  // var marker2 = new google.maps.Marker({
-  //   position: {
-  //     lat: 37.781350,
-  //     lng: -122.475883
-  //   },
-  //   map: map.gMap,
-  //   icon: 'mapicons/farmstand.png'
-  // });
+    // var marker2 = new google.maps.Marker({
+    //   position: {
+    //     lat: 37.781350,
+    //     lng: -122.475883
+    //   },
+    //   map: map.gMap,
+    //   icon: 'mapicons/farmstand.png'
+    // });
 
 }(window, window.Mapster));
